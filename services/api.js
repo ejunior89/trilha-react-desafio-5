0b9267689461bcebda345a444e105a8f4b86dbd6
Elio maia da Silva Junior <99017433+ejunior89@users.noreply.github.com>
2023-10-19 10:24:18 -0400

    .

--- services/api.js
+++ services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 export const api  = axios.create({
-    baseURL: 'https://wwxiguoourfkbupbbzwf.supabase.co/rest/v1/',
+    baseURL: 'https://wwxiguoourfkbupbbzwf.supabase.co/rest/v1',
     headers: {
-        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind3eGlndW9vdXJma2J1cGJiendmIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTc1NDk2MDksImV4cCI6MjAxMzEyNTYwOX0.giTViiO6qehROlUZq72ZqOI9XsAtz1F7WcWHvWjBC8E"
+        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind3eGlndW9vdXJma2J1cGJiendmIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTc1NDk2MDksImV4cCI6MjAxMzEyNTYwOX0.giTViiO6qehROlUZq72ZqOI9XsAtz1F7WcWHvWjBC8E",
     }
 })
